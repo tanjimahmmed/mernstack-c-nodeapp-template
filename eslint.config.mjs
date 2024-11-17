d044007ba0d47bcf05f77771b370f6cefa239134
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2024-11-17 10:39:39 +0600

    test lint stage 2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,7 +16,7 @@ export default tseslint.config(
     },
     rules: {
         // 'no-console': 'error',
-        // 'dot-notation': 'error'
+        'dot-notation': 'error'
     },
   },
 );
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 function welcome(name: string) {
-    console.log('hi');
-    
-    return name;
+    console.log('hi')
+
+    const user = {
+        name: 'Tanjim',
+    }
+
+    const fname = user.name
+
+    return name + fname
 }
 
 welcome('Tanjim')
