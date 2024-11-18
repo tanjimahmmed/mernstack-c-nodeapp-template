1a6131b92c207e840ef6bb5d9722ef0585cd1ab9
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2024-11-18 21:30:04 +0600

    add jest config js inside eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,12 @@ export default tseslint.config(
     eslint.configs.recommended,
     ...tseslint.configs.recommendedTypeChecked,
     {
-        ignores: ['dist', 'node_modules', 'eslint.config.mjs', 'jest.config.js'],
+        ignores: [
+            'dist',
+            'node_modules',
+            'eslint.config.mjs',
+            'jest.config.js',
+        ],
     },
     {
         languageOptions: {
